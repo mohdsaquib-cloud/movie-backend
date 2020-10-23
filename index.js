c4f54b54311d07572505c68a1089d46ae3575f73
Saquib <mohdsaquib9876@gmail.com>
2020-10-23 13:10:42 +0530

    movie-backend deploy

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const winston = require("winston");
 const express = require("express");
 const config = require("config");
 const app = express();
-
+const multer = require('multer');
+const path = require('path');
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
@@ -10,6 +11,17 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require('./startup/prod')(app);
+
+const storage = multer.diskStorage({
+  destination:'public/uploads',
+  filename : function(req,file,callback){
+    callback(null,file.fieldname+'-'+Date.now()+path.extname(file.originalname));
+
+  }
+})
+const upload = multer({
+  storage:storage
+}).single('myImage');
 
 
 const port = process.env.PORT || config.get("port");
--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const movies = require('../routes/movies');
 const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const upload = require('../routes/upload');
 const error = require('../middleware/error');
 module.exports = function (app) {
     app.use(express.json());
@@ -14,5 +15,6 @@ module.exports = function (app) {
     app.use('/api/rentals',rentals);
     app.use('/api/users',users);
     app.use('/api/auth',auth);
+    app.use('/api/upload',upload);
     app.use(error);    
 }
